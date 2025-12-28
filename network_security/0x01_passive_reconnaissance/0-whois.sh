b1fe8fdad4e52557e4624c9d875b418c86788571
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-12-28 12:02:46 +0000

    tasks

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/^(Registrant|Admin|Tech)/{f=$1;v=$2;sub(/^ /,"",v);if(f~/(Street)/)v=v" ";if(f~/(Phone Ext|Fax Ext)/)print f":,";else print f","v}' > "$1.csv"
+whois "$1" | awk -F: '/^(Registrant|Admin|Tech)/{k=$1;v=$2;sub(/^ /,"",v);if(k~/(Phone Ext|Fax Ext)/)print k", ";else if(v=="")print k", ";else print k", "v}' > "$1.csv"
